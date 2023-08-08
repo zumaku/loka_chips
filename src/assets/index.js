f9406d76ef695f86c54b35849acbeea11778ef4f
zumaku <60200121101@uin-alauddin.ac.id>
2023-08-08 10:56:40 +0800

    Make Assets Indexes

---
+++ src/assets/index.js
@@ -0,0 +1,53 @@
+import logo from "./horizontal_loka_chips.svg";
+import heroimg from "./hero_img.png";
+import original1 from "./original_tilt.png";
+import original2 from "./original_stand.png";
+import chocholate1 from "./chocolate_tilt.png";
+import chocholate2 from "./chocolate_stand.png";
+import balado1 from "./balado_tilt.png";
+import balado2 from "./balado_stand.png";
+import enjoy1 from "./studying.svg";
+import enjoy2 from './discussion.svg';
+import enjoy3 from './Working.svg';
+import uinimg from "./uin_img.png";
+import gallery1 from "./gallery1.png";
+import gallery2 from "./gallery2.png";
+import gallery3 from "./gallery3.png";
+import gallery4 from "./gallery4.png";
+import gallery5 from "./gallery5.png";
+import gallery6 from "./gallery6.png";
+import gallery7 from "./gallery7.png";
+import gallery8 from "./gallery8.png";
+import gallery9 from "./gallery9.png";
+import location from "./location.svg";
+import email from './email.svg';
+import world from "./world.svg";
+import instagram from './instagram.svg';
+
+export {
+    logo,
+    heroimg,
+    original1,
+    original2,
+    chocholate1,
+    chocholate2,
+    balado1,
+    balado2,
+    enjoy1,
+    enjoy2,
+    enjoy3,
+    uinimg,
+    gallery1,
+    gallery2,
+    gallery3,
+    gallery4,
+    gallery5,
+    gallery6,
+    gallery7,
+    gallery8,
+    gallery9,
+    location,
+    email,
+    world,
+    instagram
+}
