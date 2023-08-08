f0f01fbd37985dd579bc9cfd0e3eed47c708aa83
zumaku <60200121101@uin-alauddin.ac.id>
2023-08-08 19:52:25 +0800

    setup tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#F0C91A", 
+        secondary: "#341D12",
+        accent: "#DD9A00",
+        txtcolor: "#1E1E1E",
+        bgcolor: "#F2F2F2",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        gsub: ["gsub", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
 }
-
