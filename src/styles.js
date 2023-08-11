73d8d76d55d6733e1587166e47de7b54fd67bf01
zumaku <60200121101@uin-alauddin.ac.id>
2023-08-11 14:41:04 +0800

    update new style

--- src/styles.js
+++ src/styles.js
@@ -1,21 +1,21 @@
 const style = {
     // Buttons
-    btnYellow: "font-bold w-fit px-8 py-[18px] rounded-md bg-primary hover:drop-shadow-[0_4px_20px_rgba(240,201,26,.5)] hover:-translate-y-1 transition duration-150 ease-in",
-    btnChocolate: "font-bold w-fit px-8 py-[18px] rounded-md bg-secondary text-white hover:-translate-y-1 transition duration-150 ease-in",
-    btnWhite: "font-bold w-fit px-8 py-[18px] rounded-md bg-white text-secondary hover:-translate-y-1 transition duration-150 ease-in",
-    btnTransparant: "font-bold w-fit px-8 py-[18px] rounded-md hover:bg-primary border-collapse text-secondary hover:-translate-y-1 transition duration-150 ease-in border-[3px] border-secondary",
+    btnYellow: "font-bold w-fit text-base sl:text-[18px] px-4 sl:px-8 py-3 sl:py-[18px] rounded-md bg-primary hover:drop-shadow-[0_4px_20px_rgba(240,201,26,.5)] hover:-translate-y-1 transition duration-150 ease-in",
+    btnChocolate: "font-bold w-fit text-base sl:text-[18px] px-4 sl:px-8 py-3 sl:py-[18px] rounded-md bg-secondary text-white hover:-translate-y-1 transition duration-150 ease-in",
+    btnWhite: "font-bold w-fit text-base sl:text-[18px] px-4 sl:px-8 py-3 sl:py-[18px] rounded-md bg-white text-secondary hover:-translate-y-1 transition duration-150 ease-in",
+    btnTransparant: "font-bold w-fit text-base sl:text-[18px] px-4 sl:px-8 py-3 sl:py-[18px] rounded-md hover:bg-primary border-collapse text-secondary hover:-translate-y-1 transition duration-150 ease-in border-[3px] border-secondary",
 
-    heading1: "text-[64px] font-bold",
-    heading2: "text-4xl font-bold",
+    heading1: "text-4xl xs:text-[55px] xs:leading-[60px] lg:text-6xl font-bold",
+    heading2: "text-1xl sl:text-4xl font-bold",
     heading3: "text-lg font-bold",
     headingS: "font-gsub text-primary drop-shadow-[3px_3px_0_#341D12] text-stroke-4",
-    paragraph: "font-poppins text-lg font-normal",
+    paragraph: "font-poppins text-sm sm:text-base sl:text-lg font-normal",
     navbar: "text-primary font-bold drop-shadow-[1.5px_1px_0_#341D12] text-stroke-1 sm:text-stroke-half",
 
     flexCenter: "flex justify-center items-center",
     flexStart: "flex justify-center items-start",
 
-    paddingX: "lg:px-24 px-6",
+    paddingX: "lg:px-20 px-5",
 
     marginX: "sm:mx-16 mx-6",
     marginY: "sm:my-16 my-6",
