e4569baf5fcc8cb8d2436014d14fb2b9a7645320
zumaku <60200121101@uin-alauddin.ac.id>
2023-08-19 14:20:04 +0800

    Fixing the animation

--- src/components/tips.jsx
+++ src/components/tips.jsx
@@ -1,37 +1,43 @@
 import { useEffect, useRef } from "react"
 import gsap, { Back } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
 import style from "../styles"
 import { enjoy } from "../constants"
 
+gsap.registerPlugin(ScrollTrigger)
+
 const Tips = () => {
-  const tipsRef = useRef(null)
+  const startAnimating = useRef(null)
 
   useEffect(() => {
-    gsap.to(tipsRef.current, {
-      y:0,
-      duration: .8,
+    const tl = gsap.timeline({
+      duration: .5,
       ease: Back.easeOut,
       scrollTrigger: {
-        trigger: tipsRef.current,
-        start: "-200px 80%"
+        trigger: startAnimating.current,
+        start: "top 50%"
       }
+    })
+
+    enjoy.map((e, index) => {
+      tl.to("#" + e.id, {y: "0", opacity: 1}, index === 0 ? "" : "-=.3")
     })
   }, [])
 
   return (
     <div className={`${style.paddingX} w-full`}>
       <div className="max-w-[1300px] m-auto flex flex-col items-center">
-        <h1 className={`${style.heading2} my-10 sm:my-16`}>Enjoy Loka Chips</h1>
-        <div
-          ref={tipsRef}
-          className="flex flex-col sl:flex-row items-center justify-between translate-y-52"
-        >
+        <h1
+          ref={startAnimating}
+          className={`${style.heading2} my-10 sm:my-16`}
+        >Enjoy Loka Chips</h1>
+        <div className="flex flex-col sl:flex-row items-center justify-between">
           {enjoy.map((tips) => (
             <div
               key={tips.id}
               className={`${
                 tips.id === "enjoy2" ? "sl:w-1/3" : "sl:w-1/4"
-              } w-full flex flex-col items-center text-center`}
+              } w-full flex flex-col items-center text-center opacity-0 translate-y-10`}
               id={tips.id}
             >
               <img src={tips.img} className="mb-5 sl:mb-12" alt="Enjoy Loka Chips" />
