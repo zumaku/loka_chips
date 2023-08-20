d89aa5b6f4d78b2e3cfdab188d3e4e9cdfd2dc99
zumaku <60200121101@uin-alauddin.ac.id>
2023-08-20 17:56:21 +0800

    Finish scroll trigger background

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,10 +1,31 @@
+import { useEffect, useRef } from "react"
+import gsap, {ScrollTrigger, SlowMo} from "gsap/all"
 
 import style from "../styles"
 import { uinimg, white_location } from "../assets"
 
+gsap.registerPlugin(ScrollTrigger)
+
 const Banner = () => {
+
+  const bgRef = useRef(null)
+
+  useEffect(() => {
+    gsap.to(bgRef.current,{
+      backgroundPosition: '50% 100%',
+      duration: 5,
+      delay: 1,
+      ease: SlowMo.easeOut,
+      scrollTrigger: {
+        trigger: bgRef.current,
+        start: "-10% 60%",
+        scrub: true,
+      }
+    })
+  }, [])
+
   return (
-    <div className={`${style.paddingX} h-[400px] overflow-hidden w-full bg-no-repeat bg-cover bg-center relative `} style={{ backgroundImage: `url(${uinimg})` }}>
+    <div ref={bgRef} className={`${style.paddingX} h-[400px] overflow-hidden w-full bg-no-repeat bg-cover bg-top relative `} style={{ backgroundImage: `url(${uinimg})` }}>
       <div className="max-w-[1300px] h-full text-white m-auto flex flex-col justify-center items-center">
         <h2 className={`${style.heading2} mb-4`}>Gratis Ongkir</h2>
         <p className={`${style.paragraph} mb-10 sm:w-2/3 text-center`}>
