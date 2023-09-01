0b515c94637db57987337f58dbb73b6bb5127a1f
zumaku <60200121101@uin-alauddin.ac.id>
2023-09-01 22:22:31 +0800

    set darkmode, but still with same color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,24 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#F0C91A",
-        primaryHalf: "rgba(255, 199, 39, .5)",
-        secondary: "#341D12",
-        accent: "#DD9A00",
-        chocolate: "#805F0F",
-        balado: "#CB2727",
-        baladoHalf: "rgb(203, 39, 39, .5)",
-        txtcolor: "#1E1E1E",
-        bgcolor: "#F2F2F2",
+        // primary: "#F0C91A",
+        // primaryHalf: "rgba(255, 199, 39, .5)",
+        // secondary: "#341D12",
+        // accent: "#DD9A00",
+        // chocolate: "#805F0F",
+        // balado: "#CB2727",
+        // baladoHalf: "rgb(203, 39, 39, .5)",
+        // txtcolor: "#1E1E1E",
+
+        primary: "rgb(var(--color-primary))",
+        primaryHalf: "rgba(var(--color-primary-half))",
+        secondary: "rgb(var(--color-secondary))",
+        accent: "rgb(var(--color-accent)",
+        chocolate: "rgb(var(--color-chocolate))",
+        balado: "rgb(var(--color-balado))",
+        baladoHalf: "rgb(var(--color-balado-half))",
+        txtcolor: "rgb(var(--color-txtcolor))",
+        bkg: "rgb(var(--color-bkg))",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
